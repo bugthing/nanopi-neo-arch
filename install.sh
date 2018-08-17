cdb935127f2890e0a9e59bcac246b4b8698adc73
Benjamin Martin <ben.martin@smartpension.co.uk>
2018-08-17 17:51:18 +0100

    Tweaked script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 set -o xtrace
 
 SDCARD=/dev/mmcblk0
@@ -10,17 +11,17 @@ echo -e "o\nn\np\n1\n\n\nw" | sudo fdisk ${SDCARD}
 
 sudo mkfs.ext4 -O ^metadata_csum,^64bit ${SDCARD}p1
 
-mkdir mnt
+mkdir ./mnt
 
-sudo mount ${SDCARD} mnt
+sudo mount ${SDCARD}p1 ./mnt
 
 wget http://archlinuxarm.org/os/ArchLinuxARM-armv7-latest.tar.gz
 
-sudo bsdtar -xpf ArchLinuxARM-armv7-latest.tar.gz -C mnt/
+sudo bsdtar -xpf ArchLinuxARM-armv7-latest.tar.gz -C ./mnt/
 
-sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "NanoPi Neo boot script" -d boot.cmd mnt/boot/boot.scr
+sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "NanoPi Neo boot script" -d boot.cmd ./mnt/boot/boot.scr
 
-sudo umount mnt
+sudo umount ./mnt
 
 git clone git://git.denx.de/u-boot.git
 cd u-boot
@@ -31,6 +32,8 @@ make -j4 ARCH=arm CROSS_COMPILE=arm-none-eabi-
 sudo dd if=u-boot-sunxi-with-spl.bin of=${SDCARD} bs=1024 seek=8
 
 cd ..
+
+sync
 
 rm -rf ArchLinuxARM-armv7-latest.tar.gz
 rm -rf u-boot
