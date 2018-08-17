756a5c9382f2cec818d77faba87c51336a21725e
Benjamin Martin <ben.martin@smartpension.co.uk>
2018-08-17 17:27:47 +0100

    Initial script

---
+++ README.md
@@ -0,0 +1,15 @@
+NanoPi NEO Arch Linux
+---------------------
+
+Install Arch Linux on the [NanoPi NEO](http://wiki.friendlyarm.com/wiki/index.php/NanoPi_NEO)
+
+Taken from here: https://wiki.archlinux.org/index.php/Orange_Pi
+
+Prepare your Arch linux machine
+
+    $ sudo pacman -S arm-none-eabi-gcc dtc git swig uboot-tools
+
+Now run the script
+
+    $ ./install.sh
+
---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -o xtrace
+
+SDCARD=/dev/mmcblk0
+
+sudo dd if=/dev/zero of=${SDCARD} bs=1M count=8
+
+echo -e "o\nn\np\n1\n\n\nw" | sudo fdisk ${SDCARD}
+
+sudo mkfs.ext4 -O ^metadata_csum,^64bit ${SDCARD}p1
+
+mkdir mnt
+
+sudo mount ${SDCARD} mnt
+
+wget http://archlinuxarm.org/os/ArchLinuxARM-armv7-latest.tar.gz
+
+sudo bsdtar -xpf ArchLinuxARM-armv7-latest.tar.gz -C mnt/
+
+sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "NanoPi Neo boot script" -d boot.cmd mnt/boot/boot.scr
+
+sudo umount mnt
+
+git clone git://git.denx.de/u-boot.git
+cd u-boot
+git checkout tags/v2018.07
+make -j4 ARCH=arm CROSS_COMPILE=arm-none-eabi- nanopi_neo_air_defconfig
+make -j4 ARCH=arm CROSS_COMPILE=arm-none-eabi-
+
+sudo dd if=u-boot-sunxi-with-spl.bin of=${SDCARD} bs=1024 seek=8
+
+cd ..
+
+rm -rf ArchLinuxARM-armv7-latest.tar.gz
+rm -rf u-boot
+sudo rm -rf mnt
